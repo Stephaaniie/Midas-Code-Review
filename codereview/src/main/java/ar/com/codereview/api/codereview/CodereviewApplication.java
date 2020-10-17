37607763552d7528872ed17612f37e6a859f2e33
AilinCaminos <ailin.m.caminos@gmail.com>
2020-10-17 13:12:50 -0300

    Se crea proyecto SpringBoot

---
+++ codereview/src/main/java/ar/com/codereview/api/codereview/CodereviewApplication.java
@@ -0,0 +1,13 @@
+package ar.com.codereview.api.codereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodereviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodereviewApplication.class, args);
+	}
+
+}
---
+++ codereview/src/test/java/ar/com/codereview/api/codereview/CodereviewApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.codereview.api.codereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodereviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
